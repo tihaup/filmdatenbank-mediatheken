9dda6a5996bbb7e95f857be1c218a3c28dfc61f8
Timo Haupt <82437332+tihaup@users.noreply.github.com>
2026-06-14 15:09:21 +0200

    config.py mit Sendern und Einstellungen angelegt

---
+++ config.py
@@ -0,0 +1,19 @@
+# Sender die abgefragt werden
+SENDER = ["ARTE.DE", "ZDF", "ARD", "3sat"]
+
+# Lieblingsregisseure
+LIEBLINGSREGISSEURE = [
+    "Almodóvar", "Almodovar",
+    "Coen",
+    "Lanthimos",
+    "Paul Thomas Anderson",
+    "Wim Wenders", "Wenders",
+]
+
+# Bewertungsschwellen
+FARBE_SEHR_GUT = "A8D5A2"   # > 4.0
+FARBE_GUT      = "D6EFD8"   # 3.66 - 4.0
+FARBE_OK       = "FFF3CD"   # 3.4 - 3.65
+
+# Mindestlaufzeit in Sekunden
+MIN_DURATION_SEC = 3600
